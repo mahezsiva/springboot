0239585f2e3d9adb87cc5d5391f90003fbaf9ad7
mahezsiva <86237516+mahezsiva@users.noreply.github.com>
2021-07-24 14:36:38 +0530

    Add files via upload

---
+++ maven-exercise2/Child4/src/main/java/com/demo/date.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.Date;
+
+public class date {
+	
+		public static Date display()
+		{
+			Date date1=new Date();
+			return date1;
+		}
+
+
+	
+
+}
---
+++ maven-exercise2/child1/src/main/java/com/child/demo/Demo1.java
@@ -0,0 +1,13 @@
+package com.child.demo;
+
+public class Demo1 {
+	
+	public static boolean sample(String s1,String s2) {
+		return s1.equalsIgnoreCase(s2);
+	}
+	public static int sample(int date) {
+		return date;
+		
+	}
+
+}
